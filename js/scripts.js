6fdc7d693a1adb0310327f6ae53f26a27a57364c
Robert Onstott <roonstott@gmail.com>
2022-11-06 14:12:20 -0800

    refactor pizza and order objects to store lists in empty arrays rather than empty objects. Add click buttons to allow for multiple toppings per pizza, and multiple pizzas per order, submitted separately. Update UI to display pizzas in order and order total

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,19 @@
-//Business Logic
+//***Business Logic***
 
 //The Two Primary Objects: Order and Pizza
 
 function Order() {
-  this.pizzas = {},
-  this.condiments = {},
-  this.drinks = {},
-  this.others = {}
+  this.pizzas = [],
+  this.condiments = [],
+  this.drinks = [],
+  this.others = [],
+  this.toppingQueue = [],
+  this.toppingQueueNames = []
 };
 
 function Pizza(menuItem, size, style, glutenFree) {
-this.menuItem = menuItem;
-this.toppings = {},
+this.menuItem = menuItem,
+this.toppings = [],
 this.size = size,
 this.style = style,
 this.glutenFree = glutenFree
@@ -32,8 +34,8 @@ function Size(size, price) {
   this.price = parseInt(price)
 }
 
-function Crust(crust, price) {
-  this.crust = crust;
+function Style(style, price) {
+  this.style = style;
   this.price = parseFloat(price);
 }
 
@@ -54,17 +56,10 @@ function GlutenFree(price) {
 
 //Pizza methods
 
-Pizza.prototype.addTopping = function(topping) {
-  let toppingID = topping.name
-  this.toppings[toppingID] = topping;
-}
-
 Pizza.prototype.toppingCost = function () {
   let totalToppingPrice = 0;
-  let toppings = this.toppings;
-  let array = Object.keys(toppings);
-  array.forEach(function(key){
-    totalToppingPrice += toppings[key].price
+  this.toppings.forEach(function(topping){
+    totalToppingPrice += topping.price
   });
   return totalToppingPrice;
 }
@@ -74,7 +69,8 @@ Pizza.prototype.totalCost = function () {
   totalCost += this.size.price;
   totalCost += this.style.price;
   totalCost += this.glutenFree.price;
-  this.totalCost = totalCost; 
+  this.cost = totalCost;
+  return totalCost;
 }
 
 //The objects that will go into the Order properties
@@ -111,27 +107,6 @@ function Delivery(price) {
 
 //Order Methods
 
-Order.prototype.addPizza = function(pizza) {
-  pizza.totalCost();
-  let pizzaID = pizza.pizza;
-  this.pizzas[pizzaID] = pizza;
-}
-
-Order.prototype.addCondiment = function(condiment) {
-  let condimentID = condiment.name;
-  this.condiments[condimentID] = condiment;
-}
-
-Order.prototype.addDrink = function(drink) {
-  let drinkID = drink.name;
-  this.drinks[drinkID] = drink;
-}
-
-Order.prototype.addOther = function(other) {
-  let otherID = other.name;
-  this.others[otherID] = other;
-}
-
 Order.prototype.inputCustomerInfo = function(customerName, phoneNumber, tip, delivery) {
   this.customerName = customerName;
   this.phoneNumber = phoneNumber;
@@ -141,10 +116,8 @@ Order.prototype.inputCustomerInfo = function(customerName, phoneNumber, tip, del
 
 Order.prototype.pizzaCost = function () {
   let totalPizzaPrice = 0;
-  let pizzas = this.pizzas;
-  let array = Object.keys(pizzas);
-  array.forEach(function(key){
-    totalPizzaPrice += pizzas[key].totalCost;
+  this.pizzas.forEach(function(pizza){
+    totalPizzaPrice += pizza.totalCost();
   });
   return totalPizzaPrice;
 }
@@ -197,102 +170,107 @@ Order.prototype.totalCostWithTip = function () {
   return tipAmount + totalCost;
 }
 
-//Test Code Pizza 
-
-// let artichokes = new Topping("artichokes", 3);
-// let anchovies = new Topping("anchovies", 3.5);
-// let olives = new Topping("olives", 2)
-// let size = new Size("18 inch", 15);
-// let crust = new Crust("thick", 4);
-// let gF = new GlutenFree(false, 0);
-// let margharita = new Pizza("margharita", size, crust, gF);
-// margharita.addTopping(artichokes);
-// margharita.addTopping(artichokes);
-// margharita.addTopping(anchovies);
-// margharita.addTopping(olives);
-// margharita.totalCost();
-// console.log(margharita);
-// console.log(margharita.totalCost);
-
-//Test Code Order
-// let cola = new Drink("CocaCola", "XL", 3);
-// let ranch = new Condiment("ranch", .5);
-// let tShirt = new Other("T-Shirt", 15);
-// let deliver = new Delivery(true, 10);
-// let robertIsHungry = new Order ("Robert", "971-998-8234", deliver, 25);
-// robertIsHungry.addCondiment(ranch);
-// robertIsHungry.addDrink(cola);
-// robertIsHungry.addOther(tShirt);
-// robertIsHungry.addPizza(margharita);
-// console.log("***pizzaCost***", robertIsHungry.pizzaCost());
-// console.log("***drinkCost***", robertIsHungry.drinkCost());
-// console.log("***otherCost***", robertIsHungry.otherCost());
-// console.log("***condimentCost***", robertIsHungry.condimentCost());
-// console.log("***deliveryprice***", robertIsHungry.delivery.price);
-// console.log("***totalCostBeforeTip***", robertIsHungry.totalCostBeforeTip());
-// console.log("***this.totalCost***", robertIsHungry.totalCost);
-// console.log("***totalCostWithTip***", robertIsHungry.totalCostWithTip());
-
-
-
-
-//User Interface Logic
+//***User Interface Logic: ***
 
 window.addEventListener("load", function(){
-  let form = document.getElementById("form");
-  form.addEventListener("submit", handleSubmit);
+  let toppingButton = document.getElementById("addTopping");
+  toppingButton.addEventListener("click", insertTopping);
+  let pizzaButton = document.getElementById("addPizza");
+  pizzaButton.addEventListener("click", insertPizza)
 });
 
-function handleSubmit (event) {
-  event.preventDefault();
+//function insertPizza
 
-  console.log(order1);
-  //Pizza
+function insertPizza(event) {
+  event.preventDefault();
+  //Pizza:
   const menuItem = document.getElementById("menuItem").value;
   const size = document.getElementById("size").value.split(":");
-  const topping = document.getElementById("toppings").value.split(":");
   const style = document.getElementById("style").value.split(":");
   const gf = document.getElementById("gf").value;
 
   let sizeObj = new Size(size[1], size[0]);
-  let toppingObj = new Topping(topping[1], topping[0]);
-  let styleObj = new Crust(style[1], style[0]);
+  let styleObj = new Style(style[1], style[0]);
   let gfObj = new GlutenFree(gf);
 
-  //Making the Pizza object
-
   let pizzaObj = new Pizza(menuItem, sizeObj, styleObj, gfObj);
-  pizzaObj.addTopping(toppingObj);
-  
-  //Other Order Items
 
-  const drink = document.getElementById("drinks").value.split(":");
-  const condiment = document.getElementById("condiments").value.split(":");
-  const other = document.getElementById("other").value.split(":");
-  
-  let drinkObj = new Drink(drink[1], drink[0]);
-  let condimentObj = new Condiment(condiment[1], condiment[0]);
-  let otherObj = new Other(other[1], other[0]);
-  
-  //Customer Info
+  //Getting Toppings from the queue:
 
-  const name = document.getElementById("name").value;
-  const phoneNumber = document.getElementById("phoneNumber").value;
-  const tip = parseFloat(document.getElementById("tip").value);
-  const delivery = document.getElementById("delivery").value;
+  order1.toppingQueue.forEach(function(toppingObj) {
+    pizzaObj.toppings.push(toppingObj);
+  });
 
-  let deliveryObj = new Delivery(delivery);
-  
-  //Updating the Order object
+  //Add Pizza to Order, manage UI displays,  empty topping queue:
 
-  order1.addPizza(pizzaObj);
-  order1.addCondiment(condimentObj);
-  order1.addDrink(drinkObj);
-  order1.addOther(otherObj);
-  order1.inputCustomerInfo(name, phoneNumber, tip, deliveryObj);
+  order1.pizzas.push(pizzaObj);
 
-  console.log(order1);
-  console.log(order1.totalCostBeforeTip());
-  console.log(order1.totalCostWithTip());
+  const pizDisp = document.getElementById("pizzaDisplay");
+  const li = document.createElement("li");
+  li.append(`${size[1]} ${menuItem} (${style[1]}, `)
+  if (gfObj.glutenFree) {
+    li.append("Gluten Free, ")
+  };
+  order1.toppingQueueNames.forEach(function(topping){
+    li.append(` +${topping}`);
+  });
+  li.append(`) $${pizzaObj.totalCost()}`)
+  pizDisp.append(li);
 
+  const pizPriceDisp = document.getElementById("pizzaPriceDisplay");
+  pizPriceDisp.innerHTML=(`$${order1.pizzaCost()}`);
+
+  order1.toppingQueue = [];
+  order1.toppingQueueNames = [];
+  document.getElementById("toppingDisplay").innerHTML = "";
 };
+
+//function insertTopping
+
+function insertTopping(event) {
+  event.preventDefault();
+  const topDisp = document.getElementById("toppingDisplay");
+  const topping = document.getElementById("toppings").value.split(":");
+  let toppingObj = new Topping(topping[1], topping[0]);
+  if (topping[0] !== "0" && !order1.toppingQueueNames.includes(topping[1])) {
+    order1.toppingQueue.push(toppingObj);
+    order1.toppingQueueNames.push(topping[1]);
+    const li = document.createElement(("li"));
+    li.append(topping[1]);
+    topDisp.append(li);    
+  };
+};
+
+
+  
+  
+  // //Other Order Items
+
+  // const drink = document.getElementById("drinks").value.split(":");
+  // const condiment = document.getElementById("condiments").value.split(":");
+  // const other = document.getElementById("other").value.split(":");
+  
+  // let drinkObj = new Drink(drink[1], drink[0]);
+  // let condimentObj = new Condiment(condiment[1], condiment[0]);
+  // let otherObj = new Other(other[1], other[0]);
+  
+  // //Customer Info
+
+  // const name = document.getElementById("name").value;
+  // const phoneNumber = document.getElementById("phoneNumber").value;
+  // const tip = parseFloat(document.getElementById("tip").value);
+  // const delivery = document.getElementById("delivery").value;
+
+  // let deliveryObj = new Delivery(delivery);
+  
+  // //Updating the Order object
+
+  // order1.addPizza(pizzaObj);
+  // order1.addCondiment(condimentObj);
+  // order1.addDrink(drinkObj);
+  // order1.addOther(otherObj);
+  // order1.inputCustomerInfo(name, phoneNumber, tip, deliveryObj);
+
+  // console.log(order1);
+  // console.log(order1.totalCostBeforeTip());
+  // console.log(order1.totalCostWithTip());
